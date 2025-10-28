d63dbfb571d11f64ea3a25b8a9a60dc0bc88dbb0
WebBlade <monctrikua@gmail.com>
2025-10-28 15:41:07 +0200

    Second part

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Command } from "commander"
 import http from "http"
-import fs from "fs"
+import fs from "fs/promises"
 import url from "url"
 import path from "path"
 
@@ -26,21 +26,71 @@ if(!options.port || !options.host || !options.cache){
 
 const cachePath = path.resolve(options.cache)
 
-if(!fs.existsSync(cachePath)){
-    fs.mkdirSync(cachePath, {recursive: true})
-
-    console.log(`Your directory successfully created: ${cachePath}`)
-}else{
+try {
+    await fs.access(cachePath)
     console.log(`Your directory is already exists: ${cachePath}`)
+} catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.mkdir(cachePath, { recursive: true });
+      console.log(`Your directory successfully created: ${cachePath}`)
+    } else {
+      console.error('Error setting up cache directory: ', error)
+      process.exit(1);
+    }
 }
 
 const server = http.createServer(async (req, res) => {
-    try {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('ok')
-    } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' })
-        res.end('Internal server error')
+
+    const { method, url} = req
+    const imageCode = url.slice(1)
+    const filePath = path.join(cachePath, `${imageCode}.jpg`)
+
+    if(method == 'GET'){
+        try {
+            const fileData = await fs.readFile(filePath)
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+            res.end(fileData)
+        } catch (error) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+            res.end('404 Not Found')
+        }
+        
+    }else if(method == 'PUT'){
+        try {
+            const chunks = []
+
+            for await(const chunk of req){
+                chunks.push(chunk)
+            }
+
+            const fileData = Buffer.concat(chunks)
+
+            await fs.writeFile(filePath, fileData)
+
+            res.end('201 Created')
+        } catch (error) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            res.end('500 Internal Server Error')
+        }
+    }else if(method == 'DELETE'){
+        try {
+            await fs.unlink(filePath)
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end('200 Successfully deleted')
+
+        } catch (error) {
+            if(error.code == 'ENOENT'){
+                res.writeHead(404, { 'Content-Type': 'text/plain' })
+                res.end('404 Not found')
+            }else{
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('500 Internal Server Error')
+            }
+        }
+    }else{
+       res.writeHead(405, { 'Content-Type': 'text/plain' })
+       res.end('405 Method not allowed')
     }
 })
 
